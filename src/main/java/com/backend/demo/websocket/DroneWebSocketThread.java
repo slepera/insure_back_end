3106a47b1417537270b404acf48ca3c4cfe74177
Emanuele <dangelo.emanuele.94@gmail.com>
2021-07-06 17:48:19 +0200

    added GPX management

--- src/main/java/com/backend/demo/websocket/DroneWebSocketThread.java
+++ src/main/java/com/backend/demo/websocket/DroneWebSocketThread.java
@@ -5,10 +5,12 @@ import com.backend.demo.data_model.DroneOutputMessage;
 import io.jenetics.jpx.GPX;
 import io.jenetics.jpx.Track;
 import io.jenetics.jpx.TrackSegment;
+import io.jenetics.jpx.WayPoint;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 
 import java.io.IOException;
+import java.util.stream.Stream;
 
 public class DroneWebSocketThread extends Thread{
     private final WebSocketSession webSocketSession;
@@ -21,25 +23,45 @@ public class DroneWebSocketThread extends Thread{
 
     @Override
     public void run() {
+        Stream<Track> trackStream;
+
+        Stream<TrackSegment> segmentStream;
+
+        Stream<WayPoint> wayPointStream = null;
+
+        WayPoint[] wayPointsArray = null;
+
+        int count = 0;
+
         try {
-            GPX.read("gpx.xml").tracks()
-                    .flatMap(Track::segments)
-                    .flatMap(TrackSegment::points)
-                    .forEach(System.out::println);
+            trackStream = GPX.reader(GPX.Version.V10).read("./data/6.gpx").tracks();
+
+            segmentStream = trackStream.flatMap(Track::segments);
+
+            wayPointStream = segmentStream.flatMap(TrackSegment::points);
+
+            wayPointsArray = wayPointStream.toArray(WayPoint[]::new);
+
         } catch (IOException e) {
             e.printStackTrace();
         }
         while(true)
         {
-            while (isSending) {
+            int size = wayPointsArray.length;
+            while(count<size && isSending){
                 try {
                     this.webSocketSession.sendMessage(new TextMessage(
-                            new DroneOutputMessage("242", "203", "drone", "drone").getJSONObject().toString()));
+                            new DroneOutputMessage(String.valueOf(wayPointsArray[count].getLatitude().toDegrees()), String.valueOf(wayPointsArray[count].getLongitude().toDegrees()), String.valueOf(wayPointsArray[count].getElevation().get().doubleValue()), String.valueOf(wayPointsArray[count].getTime().get().toEpochSecond())).getJSONObject().toString()));
                     Thread.sleep(1000);
                 } catch (IOException | InterruptedException e) {
                     e.printStackTrace();
                 }
+                count++;
             }
+            if (count == size){
+                count = 0;
+            }
+
             try {
                 Thread.sleep(1000);
             } catch (InterruptedException e) {
