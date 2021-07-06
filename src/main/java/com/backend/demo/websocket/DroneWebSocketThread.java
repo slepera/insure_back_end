52937477ce59f07830e568e48195ed224bc9468d
slepera <salvatore.lepera@next.it>
2021-07-06 16:05:05 +0200

    added gpx library

--- src/main/java/com/backend/demo/websocket/DroneWebSocketThread.java
+++ src/main/java/com/backend/demo/websocket/DroneWebSocketThread.java
@@ -2,6 +2,9 @@ package com.backend.demo.websocket;
 
 import com.backend.demo.data_model.ChartOutputMessage;
 import com.backend.demo.data_model.DroneOutputMessage;
+import io.jenetics.jpx.GPX;
+import io.jenetics.jpx.Track;
+import io.jenetics.jpx.TrackSegment;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 
@@ -18,6 +21,14 @@ public class DroneWebSocketThread extends Thread{
 
     @Override
     public void run() {
+        try {
+            GPX.read("gpx.xml").tracks()
+                    .flatMap(Track::segments)
+                    .flatMap(TrackSegment::points)
+                    .forEach(System.out::println);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         while(true)
         {
             while (isSending) {
