a4e1d0cfcb333ed95c949f5e2135fddf1574c146
Emanuele <dangelo.emanuele.94@gmail.com>
2021-07-07 11:25:06 +0200

    added video streaming service and controller

---
+++ src/main/java/com/backend/demo/VideoStreamingService.java
@@ -0,0 +1,56 @@
+package com.backend.demo;
+
+import static java.lang.Math.min;
+
+import java.io.IOException;
+
+
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.core.io.FileUrlResource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.core.io.support.ResourceRegion;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.MediaTypeFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+@Service
+public class VideoStreamingService {
+    private static final long CHUNK_SIZE = 1000000L;
+    public ResponseEntity<ResourceRegion> getVideoRegion(String rangeHeader ,String directory) throws IOException {
+        FileUrlResource videoResource = new FileUrlResource(directory + ".mp4");
+        ResourceRegion resourceRegion = getResourceRegion(videoResource, rangeHeader);
+
+        return ResponseEntity.status(HttpStatus.PARTIAL_CONTENT)
+                .contentType(MediaTypeFactory.getMediaType(videoResource).orElse(MediaType.APPLICATION_OCTET_STREAM))
+                .body(resourceRegion);
+    }
+
+    private ResourceRegion getResourceRegion(UrlResource video, String httpHeaders) throws IOException {
+        ResourceRegion resourceRegion = null;
+
+        long contentLength = video.contentLength();
+        int fromRange = 0;
+        int toRange = 0;
+        if (StringUtils.isNotBlank(httpHeaders)) {
+            String[] ranges = httpHeaders.substring("bytes=".length()).split("-");
+            fromRange = Integer.valueOf(ranges[0]);
+            if (ranges.length > 1) {
+                toRange = Integer.valueOf(ranges[1]);
+            } else {
+                toRange = (int) (contentLength - 1);
+            }
+        }
+
+        if (fromRange > 0) {
+            long rangeLength = min(CHUNK_SIZE, toRange - fromRange + 1);
+            resourceRegion = new ResourceRegion(video, fromRange, rangeLength);
+        } else {
+            long rangeLength = min(CHUNK_SIZE, contentLength);
+            resourceRegion = new ResourceRegion(video, 0, rangeLength);
+        }
+
+        return resourceRegion;
+    }
+}
---
+++ src/main/java/com/backend/demo/rest_controllers/VideoStreamController.java
@@ -0,0 +1,34 @@
+package com.backend.demo.rest_controllers;
+
+import java.io.File;
+import java.io.IOException;
+
+import com.backend.demo.VideoStreamingService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.core.io.support.ResourceRegion;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class VideoStreamController {
+
+    @Autowired
+    VideoStreamingService service;
+    public static final String VideoUploadingDir = "./data/Platform_edit_2";
+
+    @GetMapping(value = "/video", produces = "application/octet-stream")
+    public ResponseEntity<ResourceRegion> getVideo(@RequestHeader(value = "Range", required = false) String rangeHeader)
+            throws IOException {
+
+        if (!new File(VideoUploadingDir).exists()) {
+            new File(VideoUploadingDir).mkdirs();
+        }
+        return service.getVideoRegion(rangeHeader, VideoUploadingDir);
+
+    }
+
+}
