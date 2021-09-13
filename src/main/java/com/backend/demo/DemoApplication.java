59d6647b280b64e899bf58d7ad8435e8ee545e9e
Emanuele <dangelo.emanuele.94@gmail.com>
2021-09-13 18:00:38 +0200

    added instructions to generate war file

--- src/main/java/com/backend/demo/DemoApplication.java
+++ src/main/java/com/backend/demo/DemoApplication.java
@@ -3,9 +3,10 @@ package com.backend.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class DemoApplication {
+public class DemoApplication extends SpringBootServletInitializer {
 
 
 	public static void main(String[] args) {
