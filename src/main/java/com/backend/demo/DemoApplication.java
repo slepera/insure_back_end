9528804eb4834af5ee435dd41e3878dcbabdd4ef
Salvatore Lepera <salvatore.lepera.lavoro@gmail.com>
2021-09-16 12:12:14 +0200

    commented sections for war generation in main class and in pom

--- src/main/java/com/backend/demo/DemoApplication.java
+++ src/main/java/com/backend/demo/DemoApplication.java
@@ -1,6 +1,25 @@
 package com.backend.demo;
 
 
+/* code used for war generation
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class DemoApplication extends SpringBootServletInitializer {
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+
+
+}
+*/
+
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
